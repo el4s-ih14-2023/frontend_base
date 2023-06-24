e1712703bbe5af8e912ec2f3cc7922ec1c23ee04
aogumobc <aogumobc@gmail.com>
2023-06-24 12:23:06 +0900

    ケーブルクラス

---
+++ src/components/Cable/Cable.example.ts
@@ -0,0 +1,17 @@
+// 使用例:
+const plusTerminalCoords = [1, 2, 3];
+const minusTerminalCoords = [4, 5, 6];
+
+const cable = new Cable(plusTerminalCoords, minusTerminalCoords);
+
+console.log(cable.getPlusTerminal()); // 出力: [1, 2, 3]
+console.log(cable.getMinusTerminal()); // 出力: [4, 5, 6]
+
+const newPlusTerminalCoords = [7, 8, 9];
+const newMinusTerminalCoords = [10, 11, 12];
+
+cable.setPlusTerminal(newPlusTerminalCoords);
+cable.setMinusTerminal(newMinusTerminalCoords);
+
+console.log(cable.getPlusTerminal()); // 出力: [7, 8, 9]
+console.log(cable.getMinusTerminal()); // 出力: [10, 11, 12]
