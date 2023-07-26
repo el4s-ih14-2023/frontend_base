e24cbc5ed6643d256a48a94685607a1726a32cb8
SO <78200285+sora3765@users.noreply.github.com>
2023-07-26 10:17:54 +0900

    フォーマットの調整

--- src/components/GPIOPin.tsx
+++ src/components/GPIOPin.tsx
@@ -1,61 +1,61 @@
 // import React from 'react';
 
-// interface GPIOPinProps {
-//     pinNumber: number;
-//     onClick: (pinNumber: number) => void;
-//     position: { top: number, left: number };
+// type GPIOPinProps = {
+//     pinNumber: number,
+//     onClick: (pinNumber: number) => void,
+//     position: {
+//         top: number,
+//         left: number
+//     }
 // }
 
 // const GPIOPin: React.FC<GPIOPinProps> = ({ pinNumber, onClick, position }) => {
 //     return (
-//         <button
+//         <div
 //             style={{
-//                 width: '20px',
-//                 height: '20px',
-//                 borderRadius: '50%',
-								
-//                 fontSize: '8px',
 //                 position: 'absolute',
-//                 top: position.top,
-//                 left: position.left
+//                 top: `${position.top}%`,
+//                 left: `${position.left}px`,
+//                 width: '28px',
+//                 height: '28px',
+//                 //border: '1px solid black',
+//                 borderRadius: '50%',
+//                 cursor: 'pointer'
 //             }}
 //             onClick={() => onClick(pinNumber)}
-//         >
-//             {pinNumber}
-//         </button>
+//         ></div>
 //     );
 // };
 
 // export default GPIOPin;
 
-
 import React from 'react';
 
 type GPIOPinProps = {
-    pinNumber: number,
-    onClick: (pinNumber: number) => void,
-    position: {
-        top: number,
-        left: number
-    }
-}
+  pinNumber: number;
+  onClick: (pinNumber: number) => void;
+  position: {
+    top: number;
+    left: number;
+  };
+};
 
 const GPIOPin: React.FC<GPIOPinProps> = ({ pinNumber, onClick, position }) => {
-    return (
-        <div
-            style={{
-                position: 'absolute',
-                top: `${position.top}%`,
-                left: `${position.left}px`,
-                width: '28px',
-                height: '28px',
-                //border: '1px solid black',
-                borderRadius: '50%',
-                cursor: 'pointer'
-            }}
-            onClick={() => onClick(pinNumber)}
-        ></div>
-    );
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: `${position.top}%`,
+        left: `${position.left}px`,
+        width: '20px',
+        height: '20px',
+        border: '1px solid black',
+        borderRadius: '50%',
+        cursor: 'pointer',
+      }}
+      onClick={() => onClick(pinNumber)}
+    ></div>
+  );
 };
 
 export default GPIOPin;
